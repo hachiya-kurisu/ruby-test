f8ed935fef7691ee1cf8b09213a3152bb8df657f
蜂谷栗栖 <kurisu@blekksprut.net>
2025-11-18 22:11:07 +0900

    🧹

--- app/controllers/webhooks/apple_controller.rb
+++ app/controllers/webhooks/apple_controller.rb
@@ -1,6 +1,6 @@
 module Webhooks
   class AppleController < ApplicationController
-    EVENT_TYPES = {
+    EVENTS = {
       'PURCHASE': :confirmed,
       'RENEW': :renewed,
       'CANCEL': :cancelled
@@ -8,7 +8,7 @@ module Webhooks
 
     # POST /webhooks/apple
     def create
-      # completely skip jwt handling
+      # completely skip jwt handling 🙃
       payload = request.body.read
       subscription = Subscription.find_by!(transaction_id: params[:transaction_id])
 
@@ -17,10 +17,10 @@ module Webhooks
         return head :unprocessable_entity
       end
 
-      event_type = EVENT_TYPES.fetch(params[:type].to_sym)
+      event_type = EVENTS.fetch(params[:type].to_sym)
       purchase_date = DateTime.parse(params[:purchase_date])
 
-      # Check if event already exists
+      # check if event already exists - ideally we'd have an id 🤔
       existing_event = subscription.subscription_events.find_by(
         event_type: event_type,
         transaction_id: params[:transaction_id],
@@ -28,12 +28,13 @@ module Webhooks
       )
       return head :ok if existing_event
 
+      money = Monetize.parse(params[:amount], params[:currency])
       event = subscription.subscription_events.create!(
         event_type: event_type,
         transaction_id: params[:transaction_id],
         product_id: params[:product_id],
-        amount_cents: (params[:amount].to_f * 100).round,
-        amount_currency: params[:currency],
+        amount_cents: money.cents,
+        amount_currency: money.currency.to_s,
         purchase_date: purchase_date,
         expires_date: DateTime.parse((params[:expires_date])),
         raw_payload: payload
